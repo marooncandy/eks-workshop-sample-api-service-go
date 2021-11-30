95e2e6e6c89a6881744d2fe5d52f4e0778b89c7e
marooncandy <60725076+marooncandy@users.noreply.github.com>
2021-11-29 20:35:49 -0800

    edited

--- main.go
+++ main.go
@@ -1,55 +1,44 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"io"
-	"net/http"
-	"os"
-	"sort"
-	"strings"
+    "encoding/json"
+    "fmt"
+    "io"
+    "net/http"
+    "os"
+    "sort"
+    "strings"
 )
 
+const IndexHTML = `
+<h1>hello world!</h1>
+`
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+    http.HandleFunc('/', Index)
+    http.ListenAndServe(":8080", nil)
+}
 
-		f := fib()
-
-		res := &response{Message: "Hi this is Andi :)"}
-
-		for _, e := range os.Environ() {
-			pair := strings.Split(e, "=")
-			res.EnvVars = append(res.EnvVars, pair[0]+"="+pair[1])
-		}
-		sort.Strings(res.EnvVars)
-
-		for i := 1; i <= 90; i++ {
-			res.Fib = append(res.Fib, f())
-		}
-
-		// Beautify the JSON output
-		out, _ := json.MarshalIndent(res, "", "  ")
-
-		// Normally this would be application/json, but we don't want to prompt downloads
-		w.Header().Set("Content-Type", "text/plain")
-
-		io.WriteString(w, string(out))
-
-		fmt.Println("Hello world - the log message")
-	})
-	http.ListenAndServe(":8080", nil)
+func Index(w http.ResponseWriter, r *http.Request) {
+    url := r.FormValue("url")
+    if url == "" {
+        fmt.Fprint(w, IndexHTML)
+        return
+    }
+    key := "Placeholder"
+    fmt.Fprintf(w, "http://localhost:8080/%s", key)
 }
 
 type response struct {
-	Message string   `json:"message"`
-	EnvVars []string `json:"env"`
-	Fib     []int    `json:"fib"`
+    Message string   `json:"message"`
+    EnvVars []string `json:"env"`
+    Fib     []int    `json:"fib"`
 }
 
 func fib() func() int {
-	a, b := 0, 1
-	return func() int {
-		a, b = b, a+b
-		return a
-	}
+    a, b := 0, 1
+    return func() int {
+        a, b = b, a+b
+        return a
+    }
 }
